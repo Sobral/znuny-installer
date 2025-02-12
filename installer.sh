263596117063f06bcc31414e102cee8e87737ffa
Luciano Sobral <sobral.luciano@gmail.com>
2025-02-12 18:26:48 -0300

    Download, Extract and create a symbolic link as otrs

---
+++ installer.sh
@@ -0,0 +1,13 @@
+echo "Downloading znuny"
+
+wget https://download.znuny.org/releases/znuny-6.5.10.tar.gz
+
+echo "Download completed"
+
+echo "Unpacking znuny"
+
+tar xfz znuny-6.5.10.tar.gz
+
+echo "Unpacked completed"
+
+ln -s /opt/znuny-6.5.10 /opt/otrs
